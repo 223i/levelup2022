088a9d49f76a9982b8b69641b035749ea8e4c497
irina korobeinikova <ironkorobeinikova@gmail.com>
2022-12-18 16:51:23 +0300

    Lesson 4: resolved 3d part of comments

--- src/main/java/ru/levelup/lesson2/EmployeeTreeSetComparator.java
+++ src/main/java/ru/levelup/lesson2/EmployeeTreeSetComparator.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 public class EmployeeTreeSetComparator implements Comparator<Employee> {
     @Override
     public int compare(Employee employee1, Employee employee2) {
-        if (Objects.equals(employee1.initials(), employee2.initials())){
+        if (Objects.equals(employee1.initials(), employee2.initials())) {
             return 0;
-        } else{
-            return 1;
+        } else {
+            if (employee2.workAge() >= employee1.workAge()) {
+                return 1;
+            } else {
+                return -1;
+            }
         }
     }
 }
